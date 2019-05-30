a9a35241e4de110c069897a44599813447cc4e99
Cleison França Amorim <cleison.upgrade@gmail.com>
2019-05-30 06:15:59 -0400

    course angular 7 with material in progress

---
+++ CursoAngular7/rxjs/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+import { FormsModule } from '@angular/forms';
+import { BasicCreationComponent } from './basic-creation/basic-creation.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BasicCreationComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
